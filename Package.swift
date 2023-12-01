e42b59bac33cef222b44e56671bd9b9bf8fcde66
Robert Brune <robert@brune-home.de>
2023-12-01 04:59:07 +0100

    Add minimum iOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "WledLib",
     platforms: [
-            .macOS(.v10_15) // Specify the minimum macOS version here
+            .macOS(.v10_15),
+            .iOS(.v13),
         ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
